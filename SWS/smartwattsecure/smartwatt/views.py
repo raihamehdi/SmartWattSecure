a59abd6ba134def674fe656d7b8641216cb82b68
raihamehdi <abdemehdi49@gmail.com>
2024-05-13 09:20:51 +0500

    committed

--- SWS/smartwattsecure/smartwatt/views.py
+++ SWS/smartwattsecure/smartwatt/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
+import random
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Admin,User
 from django.db.models import Q
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 def registeradmin(request):
     if request.method == 'POST':
@@ -58,3 +63,42 @@ def login(request):
 
     else:
         return render(request, 'login_signup.html')
+
+otp_storage = {}
+
+def generate_otp():
+    return str(random.randint(1000, 9999))  # Generate a 4-digit OTP
+
+def send_otp(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        # Generate OTP
+        otp = generate_otp()
+        otp_storage[email] = otp  # Store the OTP temporarily (you might want to use a more secure storage)
+
+        # Send OTP via email
+        subject = 'Your OTP for Account Verification'
+        html_message = render_to_string('emailtemp.html', {'otp': otp})
+        plain_message = strip_tags(html_message)
+        from_email = settings.EMAIL_HOST_USER  # Update with your email address or use a custom sender
+        to_email = email
+        send_mail(subject, plain_message, from_email, [to_email], html_message=html_message, fail_silently=False)
+
+        return render(request, 'enterotp.html', {'email': email, 'otp': otp})
+    else:
+        return render(request, 'forgetpass.html')
+# def verifyotp(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         entered_otp = request.POST.get('otp')
+
+#         if email in otp_storage and otp_storage[email] == entered_otp:
+#             del otp_storage[email]  # Remove the OTP after successful verification
+
+#             # Redirect to the password reset page or implement your logic here
+#             return render(request, 'resetpass.html', {'email': email})
+#         else:
+#             return render(request, 'enterotp.html', {'email': email, 'error': 'Invalid OTP. Please try again.'})
+#     else:
+#         return redirect('forgetpass')
