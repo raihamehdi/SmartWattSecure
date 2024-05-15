04fb3017a5f47fea01d335c470d2a571050d07ed
raihamehdi <abdemehdi49@gmail.com>
2024-05-15 21:22:38 +0500

    committed

--- SWS/smartwattsecure/smartwatt/views.py
+++ SWS/smartwattsecure/smartwatt/views.py
@@ -19,7 +19,7 @@ def registeradmin(request):
         email = request.POST.get('email')
 
         if Admin.objects.filter(email=email).exists():
-            return render(request, 'registeradmin.html', {'error': 'Admin with this email already exists.'})
+            return render(request, 'registeradmin.html')
         else:
             admin = Admin(admin_name=admin_name, email=email)
             admin.password = make_password(password)
@@ -37,7 +37,7 @@ def signup(request):
         email = request.POST.get('email')
         
         if Admin.objects.filter(Q(email=email) | Q(admin_name=username)).exists() or User.objects.filter(Q(email=email) | Q(username=username)).exists():
-            return render(request, 'signup.html', {'error': 'Email or username already exists.'})
+            return render(request, 'signup.html')
         else:
             user = User(username=username, email=email)
             user.password = make_password(password)
@@ -48,7 +48,7 @@ def signup(request):
         return render(request, 'signup.html')
 
 
-def login(request):
+def signin(request):
     if request.method == 'GET':
         email = request.GET.get('email')
         password = request.GET.get('password')
@@ -61,8 +61,7 @@ def login(request):
         if admin is not None and check_password(password, admin.password):
             return render(request, 'admindash.html')
 
-        error_message = 'Invalid email or password.'
-        return render(request, 'signin.html', {'error': error_message})
+        return render(request, 'signin.html')
 
     else:
         return render(request, 'signin.html')
@@ -85,8 +84,7 @@ def sendotp(request):
             request.session['reset_email'] = email
             return render(request, 'enterotp.html')
         else:
-            error_message = 'Invalid email.'
-            return render(request, 'forgetpass.html', {'error': error_message})
+            return render(request, 'forgetpass.html')
     return render(request, 'forgetpass.html')
 
 
@@ -103,7 +101,7 @@ def verifyotp(request):
             request.session['reset_email'] = email
             return render(request, 'resetpass.html')
         else:
-            return render(request, 'enterotp.html', {'error_message': 'Invalid OTP. Please try again.'})
+            return render(request, 'enterotp.html')
 
     return render(request, 'forgetpass.html')
 
@@ -120,9 +118,7 @@ def resetpass(request):
                 
                 del request.session['reset_code']
                 del request.session['reset_email']
-                return render(request, 'signin.html')
-        else:
-            print('password doesnt match')    
+                return render(request, 'signin.html')   
 
     else:
         return render(request, 'resetpass.html')
@@ -139,9 +135,7 @@ def energyc(request):
         active_power = request.POST.get('active_power')
         time = request.POST.get('time')
         user_id = request.POST.get('user_id')
-         # Fetch the user object based on the provided user_id
         user = User.objects.get(user_id=user_id)
-        # Create a new EnergyConsumption object and save it to the database
         consumption = EnergyConsumption.objects.create(
             active_power=active_power,
             time=time,
@@ -152,7 +146,6 @@ def energyc(request):
         return render(request,"consumptiondata.html" )
     
     else:
-        # Retrieve all existing EnergyConsumption objects
         consumptions = EnergyConsumption.objects.all()
         return render(request, 'consumptiondata.html', {'consumptions': consumptions})
     
