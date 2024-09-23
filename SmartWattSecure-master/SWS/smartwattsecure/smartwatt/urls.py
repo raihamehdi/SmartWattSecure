004b62c3c6985f0e3cb55282ead101c6857a5aaa
rafya3 <rafyalatif36@gmail.com>
2024-09-23 15:27:49 +0500

    rafya added

---
+++ SmartWattSecure-master/SWS/smartwattsecure/smartwatt/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import SignUpView
+from . import views
+urlpatterns = [
+path('signup/', SignUpView.as_view(), name='signup'),
+path('dashboard/', views.dashboard, name='dashboard'),
+path('update/', views.update_energy_data, name='update_energy_data'),
+path('api/energy_data/', views.energy_data_api, name='energy_data_api'),
+path('dashboard2/', views.dashboard2, name='dashboard2'), 
+
+]
---
+++ SmartWattSecure-master/SWS/smartwattsecure/smartwatt/views.py
@@ -0,0 +1,75 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.http import JsonResponse
+from django.shortcuts import render
+from .forms import CustomUserCreationForm
+from .arduino import get_mock_data, predict
+from .models import EnergyData
+from datetime import datetime
+from django.utils import timezone
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+    
+
+def dashboard(request):
+    if request.method == 'GET':
+        data = EnergyData.objects.all()
+        return render(request, 'dashboard.html', {'data': data})
+
+def update_energy_data(request):
+    if request.method == 'GET':
+        data = get_mock_data()
+        power = data['power']
+        voltage = data['voltage']
+        now = timezone.now()
+        hour = now.hour  
+        day_of_week = now.weekday()  
+        month = now.month 
+        X_test = [[power, voltage, hour, day_of_week, month]]
+
+        # Get the actual prediction from the model
+        predictions = predict(X_test)
+
+        # Determine if the data is suspicious or normal based on the prediction
+        if predictions[0] == 0:  # 'Normal' prediction
+            prediction_result = "normal"
+        elif predictions[0] == 1:  # 'High' prediction
+            prediction_result = "high"
+        else:  # 'Suspicious' prediction
+            prediction_result = "suspicious"
+
+        # Store the generated data and the prediction result in the database
+        EnergyData.objects.create(
+            current=data['current'],
+            power=data['power'],
+            voltage=data['voltage'],
+            total_units_consumed=data['total_units_consumed'],
+            prediction=prediction_result,
+            timestamp=now
+        )
+        
+        # Return the generated data and prediction result
+        return JsonResponse({'data': data, 'predictions': prediction_result})
+    return JsonResponse({'status': 'failure'}, status=400)
+
+
+
+def energy_data_api(request):
+    data = EnergyData.objects.all()
+    return JsonResponse(list(data.values()), safe=False)
+
+
+def dashboard2(request):
+    update_energy_data(request)
+    return render(request, 'dashboard2.html')
+
+
+
+
+
+
+
