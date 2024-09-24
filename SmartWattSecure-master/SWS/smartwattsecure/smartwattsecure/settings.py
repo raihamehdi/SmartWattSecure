cc6c9571133f342b0059f600e36ca271574bb6b6
raihamehdi <raihamehdi70@gmail.com>
2024-09-24 20:22:29 +0500

    managing static files

--- SmartWattSecure-master/SWS/smartwattsecure/smartwattsecure/settings.py
+++ SmartWattSecure-master/SWS/smartwattsecure/smartwattsecure/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'mystatic'
+# STATIC_ROOT = BASE_DIR / 'mystatic'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
